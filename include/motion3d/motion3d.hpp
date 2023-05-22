2df1bd225a015fd549b9c3ccf29216dde771cb5b
Markus Horn <markus.horn@uni-ulm.de>
2023-05-22 14:22:18 +0200

    [FIX] add missing MANIFEST.in

--- include/motion3d/motion3d.hpp
+++ include/motion3d/motion3d.hpp
@@ -2,7 +2,7 @@
 
 #define MOTION3D_MAJOR_VERSION 0
 #define MOTION3D_MINOR_VERSION 1
-#define MOTION3D_PATCH_VERSION 0
+#define MOTION3D_PATCH_VERSION 1
 
 #include <motion3d/common.hpp>
 #include <motion3d/containers.hpp>
